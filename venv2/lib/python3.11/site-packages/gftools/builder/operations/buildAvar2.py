b396b96e30a2a4354fb60dd9c08648dcc6f1bcca
agyeiarcher <hello@agyei.design>
2025-07-20 21:25:03 -0400

    new build script alternative

--- venv2/lib/python3.11/site-packages/gftools/builder/operations/buildAvar2.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from gftools.builder.operations import OperationBase
-
-
-class BuildAvar2(OperationBase):
-    description = "Run gftools-gen-avar2"
-    rule = "gftools-gen-avar2 --inplace $in $args"
